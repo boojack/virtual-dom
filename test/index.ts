a9f635df013f2e56db429d2158c5a87fb5ebab42
Leonardo <imrealleonardo@gmail.com>
2021-03-11 18:43:58 +0800

    -update testcase;

--- test/index.ts
+++ test/index.ts
@@ -4,9 +4,9 @@ import { VElement } from "../src/vdom/VElement";
 // NOTE: Test case
 function tick() {
   const currentTime = "It is " + new Date().toLocaleTimeString();
-  const h1 = new VElement("h1", {}, ["Hello vision"]);
-  const h2 = new VElement("h2", {}, [currentTime]);
-  const div = new VElement("div", { class: "div-container" }, [h1, h2]);
+  const br = new VElement("br", {}, []);
+  const h1 = new VElement("h1", {}, ["Hello vision", br, currentTime]);
+  const div = new VElement("div", { class: "div-container" }, [h1]);
 
   render(div, document.querySelector("div.root"));
 }
